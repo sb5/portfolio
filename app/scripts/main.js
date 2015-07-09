fd88fb7501077b08ab5b9697f2ece20e049c2fb6
justbartlett <justbartlett@gmail.com>
2015-07-08 23:49:07 -0400

    Added worked with slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,9 +2,27 @@
 
 console.log('\'Allo \'Allo!');
 
-$(document).ready(function() {
+$(document).ready(function () {
     'use strict';
-	$('#Carousel').carousel({
-	   interval: 4000
-	});
+    $('#Carousel').carousel({
+        interval: 4000
+    });
+    $('#macCarousel').carousel({
+        interval: 4000
+    });
+    $('#macCarousel').on('slid.bs.carousel', function () {
+        $('.macCarousel-target.active').removeClass('active');
+        var toSlide;
+        toSlide = $('.carousel-indicators .active').attr('data-slide-to');
+        
+        console.log(toSlide);
+        
+        $('.macCarousel-target[data-slide-to=' + toSlide + ']').addClass('active');
+    });
+    $('.macCarousel-target').on('click', function () {
+        event.preventDefault();
+        $('#macCarousel').carousel(parseInt($(this).attr('data-slide-to')));
+        $('.macCarousel-target.active').removeClass('active');
+        $(this).addClass('active');
+    });
 });
